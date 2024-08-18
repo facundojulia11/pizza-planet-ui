bd6ae5195747c7480cb64143219fa7e91115c598
facundojulia11 <facundo.julia@ioet.com>
2024-08-18 19:36:48 -0300

    ingredients refactor

--- js/ingredient/ingredient.js
+++ js/ingredient/ingredient.js
@@ -1,12 +1,3 @@
-fetch("http://127.0.0.1:5000/ingredient")
-  .then((response) => response.json())
-  .then((ingredients) => {
-    let rows = ingredients.map((element) => createIngredientTemplate(element));
-    let table = $("#ingredients tbody");
-    table.append(rows);
-  });
+import { fetchProducts } from "../product/product.js";
 
-function createIngredientTemplate(ingredient) {
-  let template = $("#ingredient-item-template")[0].innerHTML;
-  return Mustache.render(template, ingredient);
-}
+fetchProducts("ingredient");
